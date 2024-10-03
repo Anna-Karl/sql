7ab9cc3358b67858634a3759ff77dba8a8d07506
Anna-Karl <annkarlova@gmail.com>
2024-10-03 02:51:57 -0400

    Added homework 1-6

--- 02_activities/homework/homework_5.sql
+++ 02_activities/homework/homework_5.sql
@@ -9,7 +9,18 @@ Think a bit about the row counts: how many distinct vendors, product names are t
 How many customers are there (y). 
 Before your final group by you should have the product of those two queries (x*y).  */
 
-
+SELECT vendor_name, product_name, SUM(purchase_per_product) as money_per_product
+FROM(
+	SELECT DISTINCT vendor_id, product_id, original_price, (original_price * 5) as purchase_per_product, customer_id
+	FROM vendor_inventory
+	CROSS JOIN customer
+	ORDER BY vendor_id, product_id
+	) AS x
+INNER JOIN vendor as v
+ON x.vendor_id = v.vendor_id
+INNER JOIN product as p
+ON x.product_id = p.product_id
+GROUP BY x.vendor_id, x.product_id;
 
 -- INSERT
 /*1.  Create a new table "product_units". 
@@ -17,19 +28,26 @@ This table will contain only products where the `product_qty_type = 'unit'`.
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
 
-
+CREATE TEMP TABLE product_units as
+SELECT *,
+	CURRENT_TIMESTAMP as snapshot_timestamp
+FROM product
+WHERE product_qty_type = 'unit';
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
 
-
+INSERT INTO product_units (product_id, product_name, product_size, product_category, product_qty, snapshot_timestamp)
+VALUES(11, 'Pears', 'large',  1, 'kg' , CURRENT_TIMESTAMP),
+	  (12, 'Plums', 'small',  1, 'kg' , CURRENT_TIMESTAMP);
 
 -- DELETE
 /* 1. Delete the older record for the whatever product you added. 
 
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
 
-
+DELETE FROM product_units
+WHERE product_id in (11, 12);
 
 -- UPDATE
 /* 1.We want to add the current_quantity to the product_units table. 
@@ -48,4 +66,20 @@ Finally, make sure you have a WHERE statement to update the right row,
 	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
 When you have all of these components, you can run the update statement. */
 
+ALTER TABLE product_units
+ADD current_quantity INT;
 
+UPDATE product_units
+SET current_quantity = (
+    SELECT COALESCE(vi.quantity, 0) as current_quantity
+    FROM (
+        SELECT product_id, quantity,
+            ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY market_date DESC) as rn
+        FROM vendor_inventory
+    ) vi
+    WHERE vi.product_id = product_units.product_id AND vi.rn = 1
+)
+WHERE product_id IN (
+    SELECT product_id
+    FROM vendor_inventory
+);
--- 02_activities/homework/homework_6.md
+++ 02_activities/homework/homework_6.md
@@ -6,3 +6,14 @@
 <br>
 
 **Write**: Reflect on your previous work and how you would adjust to include ethics and inequity components. Total length should be a few paragraphs, no more than one page.
+
+
+As an economist with experience in the operational department of a bank in Ukraine, I've seen just how important it is to weave ethics and equity into data analysis and decision-making. Focusing on ethical banking doesn't just boost the bank's reputation and build customer trust; it also makes good economic sense. By embracing these values, banks can reduce risks and enhance long-term profitability while contributing to a fairer financial situation.
+
+Unfortunately, the legal landscape in Ukraine has always presented challenges, particularly regarding laws that may not fully protect consumer rights or data privacy. I believe the government should strictly regulate the banking industry to ensure accountability and safeguard consumers and financial institutions.
+
+One essential part of ethical banking is handling personal data related to credit cards, salaries, incomes, and expenses. Following privacy regulations closely and implementing robust data security measures is critical. This isn't just about checking boxes; it's about genuinely protecting sensitive information from breaches and unauthorized access. When customers know their data is safe, trust and reassurance in the bank are built.
+
+It's also crucial to look for biases in the data that might affect different groups unfairly. For example, we need to assess whether credit scoring algorithms favor certain income levels or genders. By identifying and addressing these biases, banks can ensure fair treatment for all customers, ultimately empowering the industry to drive positive change.
+
+Another important aspect is consistency in ethical standards inside the bank and in how we deal with clients. The values guiding our customer relationships should also apply to the workplace culture. However, sometimes, this can be tough, and employees might feel pressured to bend their principles. 
